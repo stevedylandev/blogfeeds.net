0957a831a127d47a6fe103dcbfa3e6f3e8ca9abd
Steve <contact@stevedylan.dev>
2025-09-29 09:21:47 -0400

    feat: init

---
+++ scripts/build-site.ts
@@ -0,0 +1,18 @@
+import { $ } from "bun";
+
+// Clean up dist
+await $`rm -rf site-dist`;
+// Create new dist
+await $`cp -r site site-dist`;
+// Compile tailwindcss
+await $`bunx @tailwindcss/cli -i ./site/input.css -o ./site-dist/output.css `;
+// Read index file
+const htmlContent = await Bun.file("site-dist/index.html").text();
+// Update script tags and css link
+const updatedHtml = htmlContent
+	.replace(
+		`<link rel="stylesheet" href="tailwindcss" />`,
+		`<link rel="stylesheet" href="output.css" />`,
+	);
+// Write file
+await Bun.write("site-dist/index.html", updatedHtml);
