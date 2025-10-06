eecede8634c9247b8aec4c7445935555bf26bf8e
Steve <contact@stevedylan.dev>
2025-10-05 21:53:35 -0400

    feat: added feeds page

--- scripts/build-site.ts
+++ scripts/build-site.ts
@@ -6,6 +6,7 @@ await $`rm -rf site-dist`;
 await $`cp -r site site-dist`;
 // Compile tailwindcss
 await $`bunx @tailwindcss/cli -i ./site/input.css -o ./site-dist/output.css `;
+
 // Read index file
 const htmlContent = await Bun.file("site-dist/index.html").text();
 // Update script tags and css link
@@ -16,3 +17,14 @@ const updatedHtml = htmlContent
 	);
 // Write file
 await Bun.write("site-dist/index.html", updatedHtml);
+
+// Read feeds file
+const feedsContent = await Bun.file("site-dist/feeds.html").text();
+// Update script tags and css link
+const updatedFeedsHtml = feedsContent
+	.replace(
+		`<link rel="stylesheet" href="tailwindcss" />`,
+		`<link rel="stylesheet" href="/output.css" />`,
+	);
+// Write file
+await Bun.write("site-dist/feeds.html", updatedFeedsHtml);
